4ae19c4a10e30db4b5c68ce98e18354f907607d1
ktpolanski <krzysztof_polanski@o2.pl>
2022-02-19 13:15:56 +0000

    add level notes

---
+++ src/level.ts
@@ -0,0 +1,49 @@
+import {
+	autosell,
+	itemAmount,
+	totalFreeRests,
+	use,
+	visitUrl
+} from "kolmafia";
+import {
+	$item,
+	get
+} from "libram";
+import {
+	castLibrams,
+	useDefaultFamiliar
+} from "./lib"
+import {
+	outfit,
+	outfitOyster
+} from "./outfit"
+
+
+// Make and fight as many bricko oysters as possible
+function fightOysters(): void {
+	if (get("_brickoFights") < 3) {
+		// Do we have oyster ingredients?
+		while ((itemAmount($item`bricko eye brick`)>0) && (itemAmount($item`bricko brick`)>7)) {
+			//Create the oyster
+			use(8, $item`bricko brick`);
+			useDefaultFamiliar();
+			foldIfNotHave($item`wad of used tape`);
+			outfitOyster();
+			// The autoattack should go off here
+			Macro.kill().setAutoAttack();
+			use(1, $item`bricko oyster`); runCombat(Macro.kill().toString());
+			// Flip the pearl, if possible
+			autosell(1, $item`bricko pearl`);
+		}
+	}
+}
+
+// Rest in the chateau, making and fighting oysters as quickly as we have them
+while (totalFreeRests() > get("timesRested")) {
+	// Fish for a green candy heart, then for brickos
+	castLibrams();
+	// Build and fight all the oysters you can
+	fightOysters();
+	// Finally, a chateau rest!
+	visitUrl("place.php?whichplace=chateau&action=chateau_restbox");
+}
